1176970b4cd8aebadd40bad7d5625e208967d4e5
garrettladley <garrett.ladley14@gmail.com>
2024-12-29 13:03:27 -0500

    nit: header

--- internal/xhttp/headers.go
+++ internal/xhttp/headers.go
@@ -1,8 +1,9 @@
 package xhttp
 
 const (
-	HeaderContentType     string = "Content-Type"
-	HeaderApplicationJSON string = "application/json"
-	HeaderTextHTML        string = "text/html"
-	HeaderCharsetUTF8     string = "charset=utf-8"
+	HeaderContentType             string = "Content-Type"
+	HeaderApplicationJSON         string = "application/json"
+	HeaderTextHTML                string = "text/html"
+	HeaderCharsetUTF8             string = "charset=utf-8"
+	HeaderNgrokSkipBrowserWarning string = "ngrok-skip-browser-warning"
 )
